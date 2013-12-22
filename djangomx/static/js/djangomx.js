e8700c2d12948d05516894a0cd3988450e5fdfde
Ernesto Vargas <me@netoxico.com>
2013-12-22 10:33:17 -0600

    visual ajax subscribe animations

--- djangomx/static/js/djangomx.js
+++ djangomx/static/js/djangomx.js
@@ -9,7 +9,16 @@ $(function() {
             url: '/subscribe',
             data: $('#newsletter-form').serialize(),
             success: function(data) {
-                console.log(data);
+                if (data.success === true) {
+                    var button = $('#subscribe-button');
+                    button.html('<span class="glyphicon glyphicon-ok"></span>');
+                    button.attr('type', 'button');
+                    $("input[name='email_field']").val('');
+
+                } else {
+                    $('#newsletter-form').addClass('animated bounce');
+                    setTimeout(function(){$('#newsletter-form').removeClass('animated bounce')}, 1000)
+                }
             }
         });
         return false;
