ed5758efa7772f3ff76bbabd8193cabe28dad1f0
Ernesto Vargas <me@netoxico.com>
2014-03-10 23:14:47 -0600

    some code cleanup

--- djangomx/blog/models.py
+++ djangomx/blog/models.py
@@ -10,24 +10,25 @@ from user_django.models import User
 
 
 class Category(models.Model):
-    """
-    Category Model
-    """
-    title = models.CharField(verbose_name=_(u'Título'),
-                             help_text=_(u' '),
-                             max_length=255)
-
-    slug = models.SlugField(verbose_name=_(u'Slug'),
-                            help_text=_(u'Identificador Uri'),
-                            max_length=255,
-                            unique=True)
-
-    description = models.CharField(verbose_name=_(u'Descripción'),
-                                   help_text=_(u' '),
-                                   max_length=255,
-                                   blank=True)
-
-    is_active = models.BooleanField()
+    """ Category Model """
+    title = models.CharField(
+        verbose_name=_(u'Título'),
+        help_text=_(u' '),
+        max_length=255
+    )
+    slug = models.SlugField(
+        verbose_name=_(u'Slug'),
+        help_text=_(u'Identificador Uri'),
+        max_length=255,
+        unique=True
+    )
+    description = models.CharField(
+        verbose_name=_(u'Descripción'),
+        help_text=_(u' '),
+        max_length=255,
+        blank=True
+    )
+    is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -40,36 +41,38 @@ class Category(models.Model):
 
 
 class Post(models.Model):
-    """
-    Post Model
-    """
-    title = models.CharField(verbose_name=_(u'Título'),
-                             help_text=_(u' '),
-                             max_length=255)
-
-    slug = models.SlugField(verbose_name=_(u'Slug'),
-                            help_text=_(u'Identificador Uri'),
-                            max_length=255,
-                            unique=True)
-
-    image = models.ImageField(verbose_name=_(u'Imágen'),
-                              help_text=_(u'Imagen destacada'),
-                              blank=True,
-                              upload_to="blog")
-
+    """ Post Model """
+    title = models.CharField(
+        verbose_name=_(u'Título'),
+        help_text=_(u' '),
+        max_length=255
+    )
+    slug = models.SlugField(
+        verbose_name=_(u'Slug'),
+        help_text=_(u'Identificador Uri'),
+        max_length=255,
+        unique=True
+    )
+    image = models.ImageField(
+        verbose_name=_(u'Imágen'),
+        help_text=_(u'Imagen destacada'),
+        blank=True,
+        upload_to="blog"
+    )
     content = HTMLField()
     extract = HTMLField(blank=True)
-
-    category = models.ForeignKey(Category,
-                                 verbose_name=_(u'Categoría'),
-                                 null=True,
-                                 blank=True)
-
+    category = models.ForeignKey(
+        Category,
+        verbose_name=_(u'Categoría'),
+        null=True,
+        blank=True
+    )
     author = models.ForeignKey(User, verbose_name=_(u'Autor'))
-    published_at = models.DateTimeField(verbose_name=_(u'Fecha de publicación'))
+    published_at = models.DateTimeField(
+        verbose_name=_(u'Fecha de publicación')
+    )
     likes = models.PositiveIntegerField(verbose_name=_(u'Likes'))
-
-    is_active = models.BooleanField()
+    is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -82,4 +85,9 @@ class Post(models.Model):
 
     def get_admin_url(self):
         content_type = ContentType.objects.get_for_model(self.__class__)
-        return urlresolvers.reverse("admin:%s_%s_change" % (content_type.app_label, content_type.model), args=(self.id,))
+        return urlresolvers.reverse(
+            "admin:%s_%s_change" % (
+                content_type.app_label, content_type.model
+            ),
+            args=(self.id,)
+        )
