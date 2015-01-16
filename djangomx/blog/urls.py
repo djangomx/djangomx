d3594f63e359cc81b0bd0518c911b14b46336be7
Giovanni Cortes <me@giovannicortes.com>
2015-01-15 22:23:34 -0600

    Added pagination, archives, Feed and new templates

--- djangomx/blog/urls.py
+++ djangomx/blog/urls.py
@@ -5,7 +5,23 @@ try:
 except ImportError:  # django < 1.4
     from django.conf.urls.defaults import *
 
+
+from .views import LatestEntriesFeed
+
 urlpatterns = patterns(
     'blog.views',
     url(r'^$', 'blog_home', name='blog_home'),
+
+
+
+    url(r'^categoria/(?P<category_slug>[-\w]+)/$',
+        'category',
+        name='category'),
+
+    url(r'^archivos/$', 'archives', name='archives'),
+
+    (r'^feed/$', LatestEntriesFeed()),
+
+    url(r'^entrada/(?P<slug>[-\w]+)/$', 'view_post',
+        name='view_post'),
 )
--- djangomx/blog/views.py
+++ djangomx/blog/views.py
@@ -1,9 +1,111 @@
 #coding: utf-8
+from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.syndication.views import Feed
+from django.core.urlresolvers import reverse
+
 from annoying.decorators import render_to
-from models import Post
+
+from blog.models import Category, Post
 
 
 @render_to("blog_home.html")
 def blog_home(request):
-    posts = Post.objects.filter(is_active=True)
-    return {'posts': posts}
+    posts_list = Post.objects.filter(active=True).order_by('-published_at')
+    categories = Category.objects.filter(active=True)
+    paginator = Paginator(posts_list, 5)
+
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # IF page is not an intenger, deliver first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+
+    return {
+        'posts': posts,
+        'categories': categories,
+        'paginator': paginator,
+    }
+
+
+@render_to('post.html')
+def view_post(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    categories = Category.objects.filter(active=True)
+
+    return {
+        'post': post,
+        'categories': categories,
+    }
+
+
+@render_to('category.html')
+def category(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+    posts_list = Post.objects.filter(active=True,
+                                     category=category).order_by('-pub_date')
+    categories = Category.objects.filter(active=True)
+    paginator = Paginator(posts_list, 5)
+
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # IF page is not an intenger, deliver first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+
+    return {
+        'category': category,
+        'posts': posts,
+        'categories': categories,
+        'paginator': paginator,
+    }
+
+
+@render_to('archives.html')
+def archives(request):
+    posts_list = Post.objects.filter(active=True).order_by('-pub_date')
+
+    years = {
+        '2015': [],
+    }
+
+    for year in years:
+        list_posts = []
+        for post in posts_list:
+            post_date = post.pub_date.date()
+            year_post = post_date.year
+
+            if year_post == int(year):
+                list_posts.append(post)
+
+        years[year] = list_posts
+
+    return {
+        'archives': years
+    }
+
+
+class LatestEntriesFeed(Feed):
+    title = 'El blog de Gidrek'
+    link = 'http://giovannicortes.com'
+    description = 'Todo lo que sé sobre Python y iOS'
+
+    def items(self):
+        return Post.objects.filter(active=True).order_by('-pub_date')[:10]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.content
+
+    def item_link(self, item):
+        return reverse('view_post', args=[item.slug])
