ed26fffd72cc01a9612cdeaf36ade1f57f90f737
andyosuna <andyosuna@gmail.com>
2015-12-01 19:26:25 -0700

    updates to core utils

--- djangomx/core/utils.py
+++ djangomx/core/utils.py
@@ -5,24 +5,30 @@ from datetime import datetime
 from django.utils.text import slugify
 
 
-def get_filename(extension):
+def get_filename(ext):
     """
     Returns a unique file name based on its extension parameter
     """
     ts = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    return '%s%s' % (ts, extension)
+    return '%s%s' % (ts, ext)
 
 
-def get_upload_to(instance, filename, dirname=''):
+def get_upload_to(instance, filename):
     """
-    Returns a file name and extension with a directory to upload to if provided.
+    Returns a file name, extension and directory.
     """
     name, ext = os.path.splitext(filename)
+    return '{}'.format(get_filename(ext))
 
-    if dirname:
-        return '%s/%s' % (dirname, get_filename(ext))
 
-    return '%s' % get_filename(ext)
+def generate_filepath(directory=''):
+    """
+    Returns the timestampped file name, extension and directory specified in the args.
+    """
+    try:
+        return '{}/{}'.format(directory, get_upload_to)
+    except TypeError:
+        return get_upload_to
 
 
 def truncated_slugify(string, max_length=75):
@@ -33,12 +39,13 @@ def truncated_slugify(string, max_length=75):
     if len(string) > max_length:
         truncated_string = ''
         for word in string.split(' '):
-            next_len = len(truncated_string) + len(word)
-            if next_len < max_length:
-                truncated_string += '{0}{1}'.format(word, ' ')
-            elif next_len == max_length:
-                truncated_string += '{0}'.format(word)
-                break
+            if len(word) < 1:
+                next_len = len(truncated_string) + len(word)
+                if next_len < max_length:
+                    truncated_string += '{0}{1}'.format(word, ' ')
+                elif next_len == max_length:
+                    truncated_string += '{0}'.format(word)
+                    break
 
         string = truncated_string.strip(' ')
 
